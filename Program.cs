99a97cb288a2eac93f86a2d37057c92e0a0438d1
Zolax9 <kljr9cat@anonaddy.me>
2022-08-13 11:36:35 +0100

    add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace STT_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
